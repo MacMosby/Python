642a1bb75ce8f0267d99c4477736abbe105a4222
Marc Rodenbusch <marcrodenbusch@gmail.com>
2022-10-25 17:50:20 +0200

    finished habit tracker project

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -1,17 +1,42 @@
 import requests
-import os
+from datetime import datetime
+
+USERNAME = "macmosby"
+TOKEN = "njkngnvdjbkjsn"
 
 pixela_endpoint = "https://pixe.la/v1/users"
-token = os.environ["PIXELA_USER_TOKEN"]
 
 user_parameters = {
-    "token": "token",
-    "username": "MacMosby",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-response = requests.post(url=pixela_endpoint, json=user_parameters)
-print(response.text)
+# response = requests.post(url=pixela_endpoint, json=user_parameters)
+# print(response.text)
 
-os.environ
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Running",
+    "unit": "km",
+    "type": "float",
+    "color": "sora"
+}
+
+header = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=header)
+# print(response.text)
+
+today = datetime.now().strftime("%Y%m%d")
+pixel_config = {
+    "date": today,
+    "quantity": input("How many km did you run today?")
+}
+response = requests.post(url=f"{graph_endpoint}/graph1", json=pixel_config, headers=header)
+print(response.text)
