9774f3657c8534176ee62f61d4bb07590afbb350
Marc Rodenbusch <marcrodenbusch@gmail.com>
2022-11-29 19:38:03 +0100

    selenium project started

--- SeleniumWebdriver/main.py
+++ SeleniumWebdriver/main.py
@@ -1,8 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 chrome_driver_path = "/Users/marcrodenbusch/code/chromedriver"
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://www.amazon.com")
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+driver.get("https://www.python.org")
 
 
+nav_bar = driver.find_element(by="By.ID", value="top")
+print(nav_bar)
+
+driver.quit()
