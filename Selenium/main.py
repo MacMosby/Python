ba103437d669e14aea8c551f97cb1d3d5a44dc5b
Marc Rodenbusch <marcrodenbusch@gmail.com>
2023-01-17 23:55:25 +0100

    finally managed to use selenium properly

---
+++ Selenium/main.py
@@ -0,0 +1,14 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+chrome_driver_path = "/Users/marcrodenbusch/code/chromedriver"
+options = Options()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.get("https://amazon.com")
+
+driver.close()
