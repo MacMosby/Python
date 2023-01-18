689288a61a6749ff9e4ef32017ec3bd0b11796a6
Marc Rodenbusch <marcrodenbusch@gmail.com>
2023-01-18 21:04:45 +0100

    finished first task with selenium

--- Selenium/main.py
+++ Selenium/main.py
@@ -1,21 +1,37 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 chrome_driver_path = "/Users/marcrodenbusch/code/chromedriver"
 options = Options()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get("https://amazon.com")
+driver.get("https://www.python.org")
+events = driver.find_elements(By.CSS_SELECTOR, ".event-widget li")
 
-# Main action here
+events_dict = {}
+
+for n in range(len(events)):
+    event_info = events[n].text
+    time = event_info.split("\n")[0]
+    name = event_info.split("\n")[1]
+    events_dict[n] = {
+        "time": time,
+        "name": name
+    }
+
+print(events_dict)
 
 
 
 
 
 
-driver.close()
+
+
+
+
+driver.quit()
