8865a4f621e5d6f4eb2c9ef8930b44ec8384fda2
Marc Rodenbusch <marcrodenbusch@gmail.com>
2023-01-24 19:08:20 +0100

    working on the tinder bot

---
+++ Day-50-Tinder-Bot/main.py
@@ -0,0 +1,27 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+chrome_driver_path = "/Users/marcrodenbusch/code/chromedriver"
+options = Options()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.get("https://www.tinder.com")
+# driver.maximize_window()
+
+
+time.sleep(3)
+
+log_in_button = driver.find_element(By.CLASS_NAME, "c9iqosj")
+log_in_button.click()
+
+time.sleep(5)
+
+facebook = driver.find_element(By.LINK_TEXT, "Login with Facebook")
+facebook.click()
