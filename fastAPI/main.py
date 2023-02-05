da25afe00dafaf5820b7b4bb91ad9902f33de082
Marc Rodenbusch <marcrodenbusch@gmail.com>
2023-02-05 16:56:29 +0100

    almost done with the wolt api

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Optional
 from datetime import datetime
 
 app = FastAPI()
@@ -10,12 +11,12 @@ class Order(BaseModel):
     delivery_distance: int
     number_of_items: int
     time: str
-    delivery_fee: int
+    delivery_fee: Optional[int]
 
 
-# @app.get("/")
-# def root():
-#     pass
+@app.get("/")
+def root():
+    pass
 
 
 @app.post("/v1/delivery_fee")
@@ -23,7 +24,7 @@ def calculate_delivery_fee(order: Order):
     value = order.cart_value
     distance = order.delivery_distance
     items = order.number_of_items
-    time = order.time
+    time = datetime.strptime(order.time, '%Y-%m-%dT%H:%M:%SZ')
     fee = 0
 
     # Add fee if order is smaller than 10€
@@ -34,8 +35,10 @@ def calculate_delivery_fee(order: Order):
     # Add additional distance fee
     if distance > 1000:
         distance -= 1000
-        additional_distance_fee = (((distance - 1) / 500) + 1) * 100
-        fee += additional_distance_fee
+        num_of_distance_ranges = distance / 500
+        if distance % 500 > 0:
+            num_of_distance_ranges += 1
+        fee += num_of_distance_ranges * 500
     # Add additional fee for surplus of item (more than 4)
     if items > 4:
         items_surplus = items - 4
@@ -43,8 +46,10 @@ def calculate_delivery_fee(order: Order):
         if items > 12:
             fee += 120
     # Add fee multiplication factor based on time
-    if time # = rush hour:
-        pass
+    rush_start = datetime.strptime("15:00:00", "%H:%M:%S").time()
+    rush_end = datetime.strptime("19:00:00", "%H:%M:%S").time()
+    if time.date().weekday() == 4 and rush_start <= time.time() <= rush_end:
+        fee *= 1.2
     # Set maximum fee
     if fee > 1500:
         fee = 1500
@@ -52,5 +57,4 @@ def calculate_delivery_fee(order: Order):
     if value >= 10000:
         fee = 0
     order.delivery_fee = fee
-
     return {"delivery_fee": order.delivery_fee}
